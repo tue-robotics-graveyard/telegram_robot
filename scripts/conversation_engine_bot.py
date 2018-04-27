d5c14fb0a2674866edd37b2e757bc99751fae780
Loy van Beek <loy.vanbeek@gmail.com>
2018-04-27 22:39:56 +0200

    Made ConversationEngineBot a AbstractHMIServer that passes Queries to the user and passes back the result

--- scripts/conversation_engine_bot.py
+++ scripts/conversation_engine_bot.py
@@ -15,6 +15,7 @@ Basic Echobot example, repeats messages.
 Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
+from telegram import Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import rospy
@@ -22,11 +23,24 @@ import rospy
 import actionlib
 from conversation_engine.msg import ConverseAction, ConverseGoal, ConverseFeedback, ConverseResult
 
-class ConversationEngineBot(object):
+from hmi import AbstractHMIServer, HMIResult
+from hmi.common import parse_sentence
+
+from threading import Event
+
+class ConversationEngineBot(AbstractHMIServer):
 
     def __init__(self, token, robot_name):
+        super(self.__class__, self).__init__("conversation_engine_hmi")
+
+        self.robot_name = robot_name
+
         self.ac = actionlib.SimpleActionClient("/conversation_engine", ConverseAction)
         self.ac.wait_for_server()
+
+        self._bot = None
+        self._chat_id = None
+        self._wait_for_answer = Event()
 
         # Create the EventHandler and pass it your bot's token.
         self.updater = Updater(token)
@@ -37,12 +51,36 @@ class ConversationEngineBot(object):
         # on different commands - answer in Telegram
         self.dp.add_handler(CommandHandler("start", self._start))
         self.dp.add_handler(CommandHandler("help", self._help))
+        self.dp.add_handler(CommandHandler("answer", self._answer_question))
 
         # on noncommand i.e message - echo the message on Telegram
         self.dp.add_handler(MessageHandler(Filters.text, self._accept_command))
 
         # log all errors
         self.dp.add_error_handler(self._error)
+
+    # Part of AbstractHmiServer
+    def _determine_answer(self, description, grammar, target, is_preempt_requested):
+        # Pose the question to the user via the chat
+        self._bot.send_message(chat_id=self._chat_id,
+                               text=description+" (Prefix your answer with /answer")
+
+        self._wait_for_answer.wait()
+
+        if rospy.is_shutdown() or is_preempt_requested():
+            return None
+
+        rospy.loginfo("Received string: '%s'", self._string)
+
+        semantics = {} #parse_sentence(self._string, grammar, target)
+
+        rospy.loginfo("Parsed semantics: %s", semantics)
+
+        result = HMIResult(self._string, semantics)
+        self._string = None
+        self._wait_for_answer.clear()
+
+        return result
 
     def run(self):
         # Start the Bot
@@ -56,6 +94,13 @@ class ConversationEngineBot(object):
     def _start(self, bot, update):
         rospy.loginfo("Received {}".format(update.message.text))
         update.message.reply_text('Hi! Your typed wish is my command')
+
+        assert isinstance(bot, Bot)
+        self._bot = bot
+        self._chat_id = update.message.chat_id
+
+        self._bot.send_message(chat_id=update.message.chat_id,
+                               text="I'm {}, please talk to me!".format(self.robot_name))
 
     def _help(self, bot, update):
         rospy.loginfo("Received {}".format(update.message.text))
@@ -76,6 +121,10 @@ class ConversationEngineBot(object):
         rospy.loginfo(result)
 
         update.message.reply_text(result.result_sentence)
+
+    def _answer_question(self, bot, update):
+        self._string = update.message.text
+        self._wait_for_answer.set()
 
     def _error(self, bot, update, error):
         rospy.logerr('Update "%s" caused error "%s"' % (update, error))
